2182dff60fecb266065585d8f8b6d75efea4359f
ec24186 <vinayn.1999@gmail.com>
2025-10-06 23:57:34 +0100

    client - server component

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+// to use hooks and make it work as Ui component as in react use the above
+// keywords
+
+// page.tsx - used as the root file of the profile component/folder
+// if page.tsx is not there the route will be given 404 error
+const page = () => {
+  return <div>Profile page - CLient</div>;
+};
+
+export default page;
