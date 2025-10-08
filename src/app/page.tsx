10b684932bce3e1dbf79ec9aaa9d8dedc604a296
ec24186 <vinayn.1999@gmail.com>
2025-10-08 17:55:37 +0100

    navigation hooks - client side: useRouter, usePathname, useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,27 @@
 // fetch data inside this component
 // access backend resources directly
 // secrets can be kept here
+"use client";
+import { useRouter } from "next/navigation";
 
 // You cannot
 // use hooks -> not possible
 // cannot use events
 
 export default function Home() {
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.back();
+  };
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="text-lg font-bold">Home Page @ root level</div>
+      <button
+        className="border border-white rounded p-2 cursor-pointer mt-2"
+        onClick={() => handleNavigate()}
+      >
+        Navigate to Home Component
+      </button>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,36 @@
 "use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 // to use hooks and make it work as Ui component as in react use the above
 // keywords
 
 // page.tsx - used as the root file of the profile component/folder
 // if page.tsx is not there the route will be given 404 error
 const page = () => {
-  return <div>Profile page - CLient</div>;
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const handleNavigate = () => {
+    router.push("/");
+  };
+  console.log(router);
+  console.log(pathName);
+  console.log(searchParams);
+  console.log(searchParams.get("name")); // profile?name=artemins o/p= artemis
+  console.log(searchParams.getAll("name")); // profile?name=artemis&name=fowl
+  console.log(searchParams.has("name")); // true
+  return (
+    <div>
+      <h1> Profile page - Client</h1>
+      <button
+        className="border border-white rounded p-2 cursor-pointer mt-2"
+        onClick={() => handleNavigate()}
+      >
+        Navigate to Home Component
+      </button>
+    </div>
+  );
 };
 
 export default page;
